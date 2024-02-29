e7cc5d225907d8b4148c5835eb880077bea474c4
CInkvader <chrisartmlorilla@gmail.com>
2024-02-29 11:26:41 +0800

    Add project files.

---
+++ AddressBook/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AddressBook
+{
+    public partial class MainWindow : Window
+    {
+        private ListViewItem _listViewItem;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 20; ++i)
+            {
+                _listViewItem = new ListViewItem();
+                _listViewItem.Content = "Christian" + i;
+                _listViewItem.Tag = "Lorilla";
+                lvContacts.Items.Add(_listViewItem);
+            }
+        }
+    }
+}
